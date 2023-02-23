ab26562834f5cd9bf173392254f5951f5c3f092a
juandcardx <cjuandiego83@gmail.com>
2023-02-23 13:34:39 -0500

    se agrego el diagrama de flujo y agrege el codigo de las formulas

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # area_perimetro_circulo
+
+## analisis
+
+Calcular area y perimetro de un circulo con las siguientes formulas teniendo la variable radio como entrada. formula-area-circulo  
+a = pi*r**2  p = 2pi*r
+
+## diseño 
+
+## diagrama de flujo
+
+![diagrama de flujo](diagrama_de_flujo.png "diagrama de flujo")  
---
+++ area_perimetro_circulo.py
@@ -0,0 +1,28 @@
+
+
+# entrada radio
+
+import math
+
+print("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
+print("_ _area y perimetro del circulo _ _ _")
+print("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
+
+
+# input 
+r = int(input("digite valor radio: "))
+
+# processing 
+
+a = math.pi*r*r 
+p = 2*math.pi*r  
+
+print("el area del circulo es: "+ str(a))
+print("el perimetro del circulo es: "+ str(p))
+
+# output
+
+print("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
+print("_ _ _ _ _ _ _resultado _ _ _ _ _ _ _ _")
+print("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _")
+
